4586543f5ba45f3d999d75168399d690cb8921f5
David <32614126+Davidk20@users.noreply.github.com>
2021-11-12 01:05:30 +0000

    StrStack Developed using TDD and the template from the OpStack class to build out a matching class.

--- Calculator/src/main/java/calculator/StrStack.java
+++ Calculator/src/main/java/calculator/StrStack.java
@@ -1,11 +1,58 @@
 package calculator;
 
+import java.util.EmptyStackException;
+
 /**
- * Class representation of a Stack of Strings. Used as a facade to hide unnecessary constructors and
- * methods that are not used by a stack.
+ * Class representation of a Stack of Strings. Since the shunting algorithm only uses a
+ * Stack of Strings, we provide a facade to hide details.
  *
  * @author David Kidd
  */
 public class StrStack {
+  Stack strStack = new Stack();
+  
+  /**
+   * Returns the size of the stack.
+   *
+   * @return The size of the stack.
+   */
+  private int getSize() {
+    return this.strStack.getSize();
+  }
+  
+  /**
+   * Checks if the stack is empty. Returns empty even if the stack is negative as a protection case.
+   *
+   * @return true if the stack is empty.
+   */
+  public boolean isEmpty() {
+    return getSize() <= 0;
+  }
 
+  /**
+   * Pushes onto the stack. Uses the method contained in {@code Stack}.
+   *
+   * @param string The symbol to be added onto the stack.
+   */
+  public void push(String string) {
+    strStack.push(new Entry(string));
+  }
+  
+  /**
+   * Returns and removes the top {@code String} in the stack. Uses the method contained in
+   * {@code Stack}.
+   *
+   * @return The String on top of the stack.
+   * @throws EmptyStackException Thrown if the stack is empty.
+   */
+  public String pop() throws EmptyStackException {
+    try {
+      return strStack.pop().getStr();
+    } catch (InvalidEntryTypeException e) {
+      return null; 
+      // This case never occurs as OpStack controls all inputs of the stack.
+    }
+  }
+  
+  
 }
--- Calculator/src/test/java/calculator/StrStackTest.java
+++ Calculator/src/test/java/calculator/StrStackTest.java
@@ -1,6 +1,9 @@
 package calculator;
 
+import java.util.EmptyStackException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  * Class designed to test the {@code StrStack} class.
@@ -19,8 +22,110 @@ public class StrStackTest {
    * clean stack.
    */
   @BeforeEach
-  public void initialiseOpStack() {
+  public void initialiseStrStack() {
     testStrStack = new StrStack();
   }
   
+  /**
+   * Test 1 - Test written to check that when a new {@code StrStack} is initialised, that so to is
+   * the {@code Stack} that it is using behind the facade. This tests the {@code isEmpty} method as
+   * it will return true if the stack size is found to be equal to zero.
+   */
+  @Test
+  public void initialiseEmptyStackTest() {
+    Assertions.assertEquals(true, testStrStack.isEmpty());
+  }
+ 
+  /**
+   * Test 2 - Test written to check that when a {@code String} is pushed onto the stack, this is
+   * accepted and corresponds to an increase of the size of the stack. This means that the isEmpty()
+   * method should now return false.
+   */
+  @Test
+  public void checkNonEmptyStackTest() {
+    testStrStack.push("string");
+    Assertions.assertEquals(false, testStrStack.isEmpty());
+  }
+  
+  /**
+   * Test 3 - Test written to check that when {@code pop()} is attempted on an empty stack, the
+   * {@code StackEmptyException} is thrown.
+   */
+  @Test
+  public void emptyStackPopTest() throws EmptyStackException {
+    Assertions.assertThrows(EmptyStackException.class, () -> testStrStack.pop());
+  }
+  
+  /**
+   * Test 4 - Test written to check that an entry in the {@code Stack} is correctly returned and
+   * that the type of the returned variable is a {@code String}. It is not necessary to check
+   * whether the size of the stack has decreased correctly as this will already be handled by
+   * {@code StackTest}.
+   */
+  @Test
+  public void validPopTest() {
+    testStrStack.push("String");
+    try {
+      Assertions.assertEquals("String", testStrStack.pop());
+    } catch (EmptyStackException e) {
+      // Catch statement here to safely deal with the exception however it is not expected to fail
+      // as the stack has a float pushed onto it in the test therefore it is known that there is
+      // something in the stack which can be popped off.
+      Assertions.fail(e);
+    }
+  }
+  
+  /**
+   * Test 5 - Test written to check that, when a series of Strings are pushed onto the stack, that
+   * the correct entry is popped off when called in the method.
+   */
+  @Test
+  public void correctPopOrderTest() {
+    testStrStack.push("String 1");
+    testStrStack.push("String 3");
+    testStrStack.push("String 2");
+    testStrStack.push("String 4");
+    try {
+      Assertions.assertEquals("String 4", testStrStack.pop());
+    } catch (EmptyStackException e) {
+      // Catch statement here to safely deal with the exception however it is not expected to fail
+      // as the stack has a float pushed onto it in the test therefore it is known that there is
+      // something in the stack which can be popped off.
+      Assertions.fail(e);
+    }
+  }
+  
+  /**
+   * Test 6 - Test written to check that the correct order of the stack is still followed after
+   * pushing and popping to and from the stack. This checks that a user can remove any number of
+   * entries from a stack and also add any number and the stack will still return the correct
+   * ordered element.
+   */
+  @Test
+  public void popAfterPopTest() {
+    testStrStack.push("String 1");
+    testStrStack.push("String 3");
+    testStrStack.push("String 2");
+    try {
+      testStrStack.pop();
+    } catch (EmptyStackException e) {
+      // Catch statement here to safely deal with the exception however it is not expected to fail
+      // as the stack has a float pushed onto it in the test therefore it is known that there is
+      // something in the stack which can be popped off.
+      Assertions.fail(e);
+    }
+    testStrStack.push("String 4");
+    try {
+      Assertions.assertEquals("String 4", testStrStack.pop());
+    } catch (EmptyStackException e) {
+      // Catch statement here to safely deal with the exception however it is not expected to fail
+      // as the stack has a float pushed onto it in the test therefore it is known that there is
+      // something in the stack which can be popped off.
+      Assertions.fail(e);
+    }
+  }
+  
+  /**
+   * Test 7 - Test written to check that {@code StrStack} correctly reverses the order of its stack.
+   */
 }
