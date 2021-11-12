d7fbbc07b830f1f11f1ae47eecd45220a284d8a3
David <32614126+Davidk20@users.noreply.github.com>
2021-11-12 00:00:08 +0000

    Symbol class now includes a "toSymbol" method which takes a string representation of a symbol ("+") and returns that symbol as its coresponding enum ("Symbol.PLUS").

--- Calculator/src/test/java/calculator/SymbolTest.java
+++ Calculator/src/test/java/calculator/SymbolTest.java
@@ -18,4 +18,24 @@ public class SymbolTest {
   public void validPrintTest() {
     Assertions.assertEquals("+", Symbol.PLUS.toString());
   }
+
+  /**
+   * Test 2 - Test written to validate the result of the {@code toSymbol()} method. The method takes
+   * a String representation of a {@code Symbol} and should return the matching {@code Symbol}.
+   */
+  @Test
+  public void validToSymbolTest() {
+    Assertions.assertEquals(Symbol.PLUS, Symbol.toSymbol("+"));
+  }
+
+
+  /**
+   * Test 3 - Test written to check that when a character not in the {@code Symbol} enum is passed
+   * in as an argument to {@code toSymbol()}, the {@code Symbol.INVALID} enum is correctly returned.
+   */
+  @Test
+  public void invalidToSymbolTest() {
+    Assertions.assertEquals(Symbol.INVALID, Symbol.toSymbol("F"));
+  }
+
 }
