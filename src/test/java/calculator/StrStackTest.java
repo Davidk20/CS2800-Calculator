29c0a274c7692c76811c3bbfe68b6fce5962fcd6
David <32614126+Davidk20@users.noreply.github.com>
2021-11-24 18:08:04 +0000

    Project was rebuilt and checked to fix JavaBuilder errors and SVN tree errors. All code is identical to the branch "stack-facades" but copied differently to avoid the ancestral issues which arose.

--- src/test/java/calculator/StrStackTest.java
+++ src/test/java/calculator/StrStackTest.java
@@ -6,71 +6,67 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
- * Class designed to test the {@code NumStack} class.
+ * Class designed to test the {@code StrStack} class.
  *
  * @author David Kidd
  *
  */
-public class NumStackTest {
+public class StrStackTest {
   /**
-   * Instance of {@code NumStack} used for testing.
+   * Instance of {@code StrStack} used for testing.
    */
-  private static NumStack testNumStack;
-
+  private static StrStack testStrStack;
 
   /**
-   * Initialises the {@code NumStack} as a new Stack before each test so that tests are running on a
+   * Initialises the {@code StrStack} as a new Stack before each test so that tests are running on a
    * clean stack.
    */
   @BeforeEach
-  public void initialiseNumStack() {
-    testNumStack = new NumStack();
+  public void initialiseStrStack() {
+    testStrStack = new StrStack();
   }
 
-
   /**
-   * Test 1 - Test written to check that when a new {@code NumStack} is initialised, that so to is
+   * Test 1 - Test written to check that when a new {@code StrStack} is initialised, that so to is
    * the {@code Stack} that it is using behind the facade. This tests the {@code isEmpty} method as
    * it will return true if the stack size is found to be equal to zero.
    */
   @Test
   public void initialiseEmptyStackTest() {
-    Assertions.assertEquals(true, testNumStack.isEmpty());
+    Assertions.assertEquals(true, testStrStack.isEmpty());
   }
 
   /**
-   * Test 2 - Test written to check that when a value of type {@code Float} is pushed onto the
-   * stack, this is accepted and corresponds to an increase of the size of the stack. This means
-   * that the isEmpty() method should now return false.
+   * Test 2 - Test written to check that when a {@code String} is pushed onto the stack, this is
+   * accepted and corresponds to an increase of the size of the stack. This means that the isEmpty()
+   * method should now return false.
    */
   @Test
   public void checkNonEmptyStackTest() {
-    testNumStack.push(1.2f);
-    Assertions.assertEquals(false, testNumStack.isEmpty());
+    testStrStack.push("string");
+    Assertions.assertEquals(false, testStrStack.isEmpty());
   }
-
 
   /**
    * Test 3 - Test written to check that when {@code pop()} is attempted on an empty stack, the
    * {@code StackEmptyException} is thrown.
-   *
    */
   @Test
   public void emptyStackPopTest() throws EmptyStackException {
-    Assertions.assertThrows(EmptyStackException.class, () -> testNumStack.pop());
+    Assertions.assertThrows(EmptyStackException.class, () -> testStrStack.pop());
   }
 
   /**
    * Test 4 - Test written to check that an entry in the {@code Stack} is correctly returned and
-   * that the type of the returned variable is a {@code Float}. It is not necessary to check whether
-   * the size of the stack has decreased correctly as this will already be handled by
+   * that the type of the returned variable is a {@code String}. It is not necessary to check
+   * whether the size of the stack has decreased correctly as this will already be handled by
    * {@code StackTest}.
    */
   @Test
-  public void popTest() {
-    testNumStack.push(1.2f);
+  public void validPopTest() {
+    testStrStack.push("String");
     try {
-      Assertions.assertEquals(1.2f, testNumStack.pop());
+      Assertions.assertEquals("String", testStrStack.pop());
     } catch (EmptyStackException e) {
       // Catch statement here to safely deal with the exception however it is not expected to fail
       // as the stack has a float pushed onto it in the test therefore it is known that there is
@@ -80,17 +76,17 @@ public class NumStackTest {
   }
 
   /**
-   * Test 5 - Test written to check that, when a series of floats are pushed onto the stack, that
+   * Test 5 - Test written to check that, when a series of Strings are pushed onto the stack, that
    * the correct entry is popped off when called in the method.
    */
   @Test
   public void correctPopOrderTest() {
-    testNumStack.push(1.0f);
-    testNumStack.push(1.1f);
-    testNumStack.push(1.2f);
-    testNumStack.push(1.3f);
+    testStrStack.push("String 1");
+    testStrStack.push("String 3");
+    testStrStack.push("String 2");
+    testStrStack.push("String 4");
     try {
-      Assertions.assertEquals(1.3f, testNumStack.pop());
+      Assertions.assertEquals("String 4", testStrStack.pop());
     } catch (EmptyStackException e) {
       // Catch statement here to safely deal with the exception however it is not expected to fail
       // as the stack has a float pushed onto it in the test therefore it is known that there is
@@ -107,21 +103,20 @@ public class NumStackTest {
    */
   @Test
   public void popAfterPopTest() {
-    testNumStack.push(1.0f);
-    testNumStack.push(1.1f);
-    testNumStack.push(1.2f);
+    testStrStack.push("String 1");
+    testStrStack.push("String 3");
+    testStrStack.push("String 2");
     try {
-      testNumStack.pop();
+      testStrStack.pop();
     } catch (EmptyStackException e) {
       // Catch statement here to safely deal with the exception however it is not expected to fail
       // as the stack has a float pushed onto it in the test therefore it is known that there is
       // something in the stack which can be popped off.
       Assertions.fail(e);
     }
-    testNumStack.push(1.3f);
-    testNumStack.push(1.4f);
+    testStrStack.push("String 4");
     try {
-      Assertions.assertEquals(1.4f, testNumStack.pop());
+      Assertions.assertEquals("String 4", testStrStack.pop());
     } catch (EmptyStackException e) {
       // Catch statement here to safely deal with the exception however it is not expected to fail
       // as the stack has a float pushed onto it in the test therefore it is known that there is
@@ -130,4 +125,21 @@ public class NumStackTest {
     }
   }
 
+  /**
+   * Test 7 - Test written to check that {@code StrStack} correctly reverses the order of its stack.
+   * When a stack (a, b, c, d) is passed in, it should return the stack (a, b, c, d) when popped. as
+   * it would usually return (d, c, b, a) when popped but this is reversed in this instance.
+   */
+  @Test
+  public void reversalTest() {
+    testStrStack.push("a");
+    testStrStack.push("b");
+    testStrStack.push("c");
+    testStrStack.push("d");
+    testStrStack.reverse();
+    Assertions.assertEquals("a", testStrStack.pop());
+    Assertions.assertEquals("b", testStrStack.pop());
+    Assertions.assertEquals("c", testStrStack.pop());
+    Assertions.assertEquals("d", testStrStack.pop());
+  }
 }
