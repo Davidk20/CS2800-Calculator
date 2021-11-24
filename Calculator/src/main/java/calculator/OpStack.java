c0f0ea3d7eff3ba2378db7c0c7d8bc0164aa663f
David <32614126+Davidk20@users.noreply.github.com>
2021-11-24 19:57:10 +0000

    Stack-facade merged with trunk.

---
+++ Calculator/src/main/java/calculator/OpStack.java
@@ -0,0 +1,63 @@
+package calculator;
+
+import java.util.EmptyStackException;
+
+/**
+ * Class representation of a Stack of Symbols (Operators). Since the shunting algorithm only uses a
+ * Stack of symbols, we provide a facade to hide details.
+ *
+ * @author David Kidd
+ */
+public class OpStack {
+  /**
+   * The stack being protected by the facade {@code OpStack}.
+   */
+  Stack opStack = new Stack();
+  
+  
+  /**
+   * Returns the size of the stack.
+   *
+   * @return The size of the stack.
+   */
+  private int getSize() {
+    return this.opStack.getSize();
+  }
+  
+  /**
+   * Checks if the stack is empty. Returns empty even if the stack is negative as a protection case.
+   *
+   * @return true if the stack is empty.
+   */
+  public boolean isEmpty() {
+    return getSize() <= 0;
+  }
+
+  
+  /**
+   * Pushes onto the stack. Uses the method contained in {@code Stack}.
+   *
+   * @param symbol The symbol to be added onto the stack.
+   */
+  public void push(Symbol symbol) {
+    opStack.push(new Entry(symbol));
+  }
+  
+  
+  /**
+   * Returns and removes the top {@code Symbol} in the stack. Uses the method contained in
+   * {@code Stack}.
+   *
+   * @return The symbol on top of the stack.
+   * @throws EmptyStackException Thrown if the stack is empty.
+   */
+  public Symbol pop() throws EmptyStackException {
+    try {
+      return opStack.pop().getSymbol();
+    } catch (InvalidEntryTypeException e) {
+      return Symbol.INVALID; 
+      // This case never occurs as OpStack controls all inputs of the stack.
+    }
+  }
+
+}
